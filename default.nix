a1e5b7a1ade0f2ccea5d368e46cc6512b3033469
r6915ee <astergthefourth@gmail.com>
2026-02-19 20:17:53 -0800

    build: inherit version from Cargo.toml for Nix package

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
+let
+  manifest = pkgs.lib.fromTOML (builtins.readFile ./Cargo.toml);
+  version = manifest.package.version;
+in
 pkgs.rustPlatform.buildRustPackage {
   pname = "templato";
-  version = "0.1.1";
+  inherit version;
   src = pkgs.lib.cleanSource ./.;
   cargoLock.lockFile = ./Cargo.lock;
 }
